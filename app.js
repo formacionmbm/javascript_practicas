f51768eacc138f6aece02f855ed4ffec2dae39b2
profe13 <formacionmbm@gmail.com>
2024-11-08 13:27:37 +0100

    primeras practicas de javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+
+window.onload=() =>{
+
+    function toUpperCase(){
+        let texto=document.getElementById("texto").value;
+        //Cambiamos el texto a mayúsculas
+        document.getElementById("texto").value=texto.toUpperCase()
+    
+    }
+    function toUpperCasePropioElemento(element){   
+        //Cambiamos el texto a mayúsculas
+        element.value=element.value.toUpperCase()
+    
+    }
+
+    let elemento = document.getElementById("texto");
+    /*
+    Manera chapucera*/
+    /*
+    elemento.addEventListener("keyup",function(){
+    toUpperCase();
+    });
+    /*/
+    elemento.addEventListener("keyup",(event) =>{    
+        event.target.value=event.target.value.toUpperCase();
+    });
+}
